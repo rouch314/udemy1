a338e4824845f4db5b5f91010b0183667a6abc29
Sergei <example@gmail.com>
2024-07-15 16:38:20 +0500

    first NEW commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert('Hello!');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt("Как Вас зовут?", "");
+answers[1] = prompt("Сколько Вам лет?", "");
+answers[2] = prompt("Где Вы работаете?", "");
+
+document.write(answers);
