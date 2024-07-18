4cee81d0d6a75b4b23b95470b424e1210d25821b
Sergei <example@gmail.com>
2024-07-18 17:27:15 +0500

    third task

--- script.js
+++ script.js
@@ -1,6 +1,89 @@
+// "use strict";
+
+// let numberOfFilms; 
+
+// function start() {
+//   numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+//   while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+//     numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+//   }
+
+// }
+
+// start();
+
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false
+// };
+
+// function rememberMyFilms() {
+// for (let i = 0; i < 2; i++) {
+//   const a = prompt('Один из последних просмотренных фильмов', ''),
+//      b = prompt('На сколько оцените его?', '');
+
+//      if (a !== null && b !== null && a!== '' && a.length < 50) {
+
+//       personalMovieDB.movies[a] = b;
+
+//       console.log('done');
+//      } else {
+//       console.log('error');
+//       i--;
+//     } 
+//   }
+// }
+
+// rememberMyFilms();
+
+// function detectPersonalLevel() {
+//   if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+//   } else if ( 10 < personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+//   } else if (personalMovieDB.count) {
+//     console.log('Вы киноман');
+//   } else {
+//     console.log('Произошла ошибка');
+//   }  
+// }
+
+// detectPersonalLevel();
+
+// function showMyDB(hidden) {
+//   if (!hidden) {
+//     console.log(personalMovieDB);
+//   }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+// function writeYourGenres() {
+//   for (let i = 1; i <= 3; i++) {
+//   const askUser = prompt(`Ваш любимый жанр под номером ${i}`);
+//   personalMovieDB.genres[i - 1] = askUser;
+//   }
+// }
+
+// writeYourGenres();
+
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+let numberOfFilms; 
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -10,30 +93,50 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2; i++) {
-  const a = prompt('Один из послединх просмотренных фильмов', ''),
-     b = prompt('На сколько оцените его?', '');
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов', ''),
+          b = prompt('На сколько оцените его?', '');
 
-     if (a !== null && b !== null && a!== '' && a.length < 50) {
-
+    if (a !== null && b !== null && a !== '' && a.length < 50) {
       personalMovieDB.movies[a] = b;
-
       console.log('done');
-     } else {
+    } else {
       console.log('error');
       i--;
-    } 
+    }
+  }
 }
 
-if (personalMovieDB.count < 10) {
-  console.log('Просмотрено довольно мало фильмов');
-} else if ( 10 < personalMovieDB.count < 30) {
-  console.log('Вы классический зритель');
-} else if (personalMovieDB.count) {
-  console.log('Вы киноман');
-} else {
-  console.log('Произошла ошибка');
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+  } else {
+    console.log('Произошла ошибка');
+  }
 }
 
+detectPersonalLevel();
 
-console.log(personalMovieDB);
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) { // <= instead of <
+    const askUser = prompt(`Ваш любимый жанр под номером ${i}`);
+    personalMovieDB.genres[i - 1] = askUser;
+  }
+}
+
+writeYourGenres();
---
+++ task.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function showFirstMessage(a, b) {
+  console.log(a + b);
+}
+
+showFirstMessage(3, 5);
+
