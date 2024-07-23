cb80024216af75c9d6f31c9000dfab72c38298d8
Sergei <example@gmail.com>
2024-07-23 16:54:57 +0500

    task number 4

--- task.js
+++ task.js
@@ -1,8 +1,27 @@
 "use strict"
 
-function showFirstMessage(a, b) {
-  console.log(a + b);
-}
+let str = "some";
 
-showFirstMessage(3, 5);
+let strObj = new String(str);
 
+console.dir([1, 2, 3, 4])
+
+const soldier = {
+  health: 400,
+  armor: 100,
+  sayHello: function() {
+    console.log("Hello!");
+  }
+};
+
+const john = Object.create(soldier);
+
+// const john = {
+//   health: 100
+// }
+
+// john.__proto__ = soldier;
+
+Object.setPrototypeOf(john, soldier);
+
+john.sayHello();
